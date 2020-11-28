d504d2df491badd1d20d7b2fd3c2c2c51234f067
iScepsis <fed_smolensk@mail.ru>
2020-11-29 00:09:10 +0300

    Add mechanism for load param components

--- src/components/ParamsPanel.vue
+++ src/components/ParamsPanel.vue
@@ -1,9 +1,14 @@
 <template>
     <div>
-        <component v-bind:is="component" />
-        <button v-on:click="toggle">Toggle</button>
-        <p>{{tool.id}}</p>
-        <p>{{tool.type}}</p>
+        <div v-if="currentTool">
+            <component v-bind:is="currentTool" />
+            <button v-on:click="toggle">Toggle</button>
+            <p>{{tool.id}}</p>
+            <p>{{currentTool}}</p>
+        </div>
+        <div v-else>
+            <p>Component not loaded</p>
+        </div>
     </div>
 </template>
 
@@ -16,6 +21,22 @@
         components: {
             Playback,
             CheckCondition
+        },
+        computed: {
+            //Получаем название модуля для подгрузки
+            currentTool: function() {
+                let componentName = '';
+                if (this.tool.type) {
+                    componentName = this.tool.type.substr(5);
+                    componentName = componentName.substr(0,  componentName.length - 5);
+                    if (!(componentName in this.$options.components)) {
+                        console.warn('Компонент с именем ' + componentName + ' не найдет в списке. ' +
+                            'Проверьте, что он был подключен или имеет верный формат имени');
+                        componentName = '';
+                    }
+                }
+                return componentName;
+            }
         },
         props: ['tool'],
         data () {
